f794707d40acc9ed6932d0ea657498d5ce34ab23
lucas <lucazs.xis@gmail.com>
2025-02-17 09:52:51 -0300

    Add files via upload

---
+++ ex015/index.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste de mesa</title>
+</head>
+<body>
+    <?php 
+    
+    $a = 10;
+    $b = 5;
+    $c = 3;
+    
+    
+    $a = $a + $b; //$a = 10 + 5 = 15    
+    $b = $b * $c; //$b = 5 - 3 = 15
+    $c = $a + $c; //$c = 10 - 3 = 12
+    
+    echo "$a, $b $c"; //exibe os valores de $a $b e $c
+    
+    
+    
+    ?>
+</body>
+</html>
